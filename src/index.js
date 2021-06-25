a67ce6db8a59d7dee50a5754629c0e9a682c9711
Aashish <you@example.com>
2021-06-25 14:44:43 +0530

    Add store and Dispatching actions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import App from './components/App';
-import './index.css'
+import './index.css';
+import movies from './reducers';
+
+const store =createStore(movies);
+console.log('store',store);
+
+store.dispatch ({
+  type:'ADD_MOVIES',
+  movies:[{name:'Superman'}]
+});
+
+console.log(store.getState());
 
 ReactDOM.render(
   <App />,
